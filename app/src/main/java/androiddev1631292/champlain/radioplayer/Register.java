a17d1c3f7d11af51b54300b55940db4e20485577
Geukers <pg.gg.7@hotmail.com>
2018-11-15 14:17:01 -0500

    Added the register page

---
+++ app/src/main/java/androiddev1631292/champlain/radioplayer/Register.java
@@ -0,0 +1,13 @@
+package androiddev1631292.champlain.radioplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
